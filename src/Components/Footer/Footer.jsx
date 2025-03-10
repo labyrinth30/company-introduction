623fca0e3e9f594e46823d92ba792a3834a9c10b
younha00 <ayeon0@office.skhu.ac.kr>
2025-03-10 18:30:43 +0900

    게시판 삭제

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -27,8 +27,6 @@ const Footer = () => {
               <li><Link to="/" onClick={scrollToTop} className="hover:text-white transition-colors">홈</Link></li>
               <li><Link to="/about" onClick={scrollToTop} className="hover:text-white transition-colors">회사 정보</Link></li>
               <li><Link to="/leadership" onClick={scrollToTop} className="hover:text-white transition-colors">임원 소개</Link></li>
-              <li><Link to="/board" onClick={scrollToTop} className="hover:text-white transition-colors">업무 게시판</Link></li>
-              <li><Link to="/our-services" onClick={scrollToTop} className="hover:text-white transition-colors">제공 기술</Link></li>
               <li><Link to="/contact" onClick={scrollToTop} className="hover:text-white transition-colors">문의</Link></li>
             </ul>
           </div>
